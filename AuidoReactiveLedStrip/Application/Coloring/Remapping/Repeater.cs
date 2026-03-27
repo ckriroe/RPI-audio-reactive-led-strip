e45537ff51768e24e7190a94c4a1f0caeac87bc0
Chriroe <ckriroe@gmail.com>
2026-03-27 18:46:09 +0100

    Fixed missing pixels with effect repeating

--- AuidoReactiveLedStrip/Application/Coloring/Remapping/Repeater.cs
+++ AuidoReactiveLedStrip/Application/Coloring/Remapping/Repeater.cs
@@ -10,17 +10,22 @@ namespace Application.Coloring.Remapping
             if (dynamicSettings.EffectRepeats == 1)
                 return colors;
 
-            return this.RepeatArray(colors, dynamicSettings.EffectRepeats);
+            return this.RepeatArray(colors, dynamicSettings.EffectRepeats, dynamicSettings.PhysicalLedCount);
         }
 
-        public Color[] RepeatArray(Color[] source, int times)
+        public Color[] RepeatArray(Color[] source, int times, int minLength)
         {
             int len = source.Length;
-            Color[] result = new Color[len * times];
+            Color[] result = new Color[minLength];
 
             for (int i = 0; i < times; i++)
             {
                 Array.Copy(source, 0, result, i * len, len);
+            }
+
+            for (int i = source.Length * times; i < minLength; i++)
+            {
+                result[i] = Color.Black;
             }
 
             return result;
